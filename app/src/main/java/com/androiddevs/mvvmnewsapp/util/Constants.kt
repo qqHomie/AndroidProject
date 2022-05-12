98a7a8fdb1ec169680b2a5c10bfffa7ea8e489a9
qqHomie <n_zhaksylykov@kbtu.kz>
2022-05-12 08:08:06 +0600

    Project

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "6f36a3b8dbf346a1ba15df10d299b4f7"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
